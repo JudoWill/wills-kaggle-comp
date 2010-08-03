0fa00a9da7779d7f856ff3ed24c90f36bead360c
Will Dampier <judowill@gmail.com>
2010-08-03 19:43:06 -0400

    added a writing function

--- chess/process.py
+++ chess/process.py
@@ -29,10 +29,10 @@ class Player():
         return max(0, self.rank-bp.rank)
 
 
-def TrainModel(csv_gen):
+def TrainModel(csv_gen, default_rank = 0.5):
     """Trains the model based on receiving a 'csv-generator' from the rows"""
 
-    player_dict = defaultdict(lambda : Player())
+    player_dict = defaultdict(lambda : Player(rank = default_rank))
 
     for row in csv_gen:
         p1 = int(row["White Player #"])
@@ -55,7 +55,7 @@ def EvaluateModel(model_dict, csv_gen):
         p2 = int(row["Black Player #"])
         s = float(row["Score"])
         score = model_dict[p1].get_match_score(model_dict[p2])
-        predicted_agg[(row['Month #'], p1)] += score
+        predicted_agg[(row['Month #'], p1, p2)] += score
         predicted_agg[(row['Month #'], p2)] += score
 
         correct_agg[(row['Month #'], p1)] += s
@@ -67,6 +67,15 @@ def EvaluateModel(model_dict, csv_gen):
 
     return sqrt(mse)
 
+def WritePrediction(model_dict, csv_gen, out_handle):
+
+    out_handle.write('"Month #","White Player #","Black Player #","Score"\n')
+    for row in csv_gen:
+        p1 = int(row["White Player #"])
+        p2 = int(row["Black Player #"])
+        m = row['Month #']
+        score = model_dict[p1].get_match_score(model_dict[p2])
+        out_handle.write('%s,%i,%i,%f\n' % (m, p1, p2, score))
 
 
 
@@ -75,7 +84,7 @@ if __name__ == '__main__':
 
     INIT_DATA_FILE = 'initial-data/training_data.csv'
     TEST_DATA_FILE = 'initial-data/test_data.csv'
-
+    OUTFILE = 'results.csv'
 
 
     with open(INIT_DATA_FILE) as handle:
@@ -83,11 +92,16 @@ if __name__ == '__main__':
 
     ntrain = int(0.7*len(train_rows))
     
-    model = TrainModel(train_rows[:ntrain])
+    model = TrainModel(train_rows[:ntrain], default_rank = 0.7)
     
     print EvaluateModel(model, train_rows[ntrain+1:])
 
+    rmodel = TrainModel(train_rows)
 
+    with open(TEST_DATA_FILE) as thandle:
+        csv_gen = csv.DictReader(thandle)
+        with open(OUTFILE) as ohandle:
+            WritePrediction(rmodel, csv_gen, ohandle)
 
 
 
