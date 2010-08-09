37afe66091a0d54b628c9a22438e9727293b5033
Will Dampier <dampier.william@gmail.com>
2010-08-09 14:44:48 -0400

    changed pretty much everything about how it predicts

--- chess/process.py
+++ chess/process.py
@@ -2,45 +2,123 @@ from __future__ import division
 import csv
 from math import sqrt
 from collections import defaultdict
+from operator import attrgetter
 
 
 
 class Player():
-    def __init__(self, rank = 0.5):
+    def __init__(self, pid, rank = 0):
         self.rank = rank
+		self.pid = pid
+        self.wins = list()
+        self.loses = list()
 
     def match(self, bp, score):
         """Updates the rank based on the rank of Black-Player and the score"""
 
-        if score > 0.5 and bp.rank > self.rank:
+        if score > 0 and bp.rank > self.rank:
             #already correct
             return
-        elif score < 0.5 and self.rank > bp.rank:
+        elif score < 0 and self.rank > bp.rank:
             #already correct
             return
         else:
             self.rank = max(self.rank+(score - bp.rank - self.rank)/2, 0)
+        
+        if score < 0:
+			self.wins.append(bp.pid)
+			bp.loses.append(self.pid)
+		elif score > 0:
+			self.loses.append(bp.pid)
+			bp.wins.append(self.pid)
 
 
 
-    def get_match_score(self, bp):
-        """Returns the 'score' of this player vs. Black-Player"""
+class PlayerDict():
+	
+	def __init__(self, default_rank = 0):
+		self.pdict = {}
+		self.default_rank = default_rank
+		self.match_list = []
+		self.ranked_list = []
+		
+	def __getitem__(self, pid):
+		
+		if pid in self.pdict:
+			return self.pdict[pid]
+		else:
+			nplayer = Player(pid, rank = self.default_rank)
+			self.pdict[pid] = nplayer
+			return nplayer
+			
+	def GetMatchScore(self, wid, bid):
+		
+		p1 = self[wid]
+		p2 = self[bid]
+		
+		return OutTreatScore(p1.rank - p2.rank)
+		
+	def PerformMatch(self, wid, bid, score):
+		
+		self[wid].match(self[bid])
+		self.match_list += [(wid, bid, score)]
+		
+	def PlayerCmp(self, wplayer, bplayer):
+		
+		value = 0
+		
+		bplayer_wins = len([i for i in bplayer.wins if i == wplayer.pid])
+		wplayer_wins = len([i for i in wplayer.wins if i == bplayer.pid])
+		
+		win_adjust = (bplayer_wins - wplayer_wins)/(bplayer_wins+wplayer_wins)
+		value += win_adjust
+		
+		rank_adjust = wplayer.rank - bplayer.rank
+		value += rank_adjust
+		
+		return value
+	
+	def SetRanks(self, num_iter = 1):
+		
+		self.ranked_list = self.pdict.values()
+		self.ranked_list.sort(key = attrgetter('rank'))
+		
+		for _ in range(num_iter):
+			self.ranked_list.sort(cmp = self.PlayerCmp)
+			self._adjust_ranks()
+			
+	def _adjust_ranks(self):
+		
+		c=0
+		while c < len(self.ranked_list)-2:
+			it1 = self.ranked_list[c]
+			it2 = self.ranked_list[c+1]
+			
+			if it2.rank > it1.rank:
+				adj = (it2.rank-it1.rank)/2
+				it2.rank -= adj
+				it1.rank += adj
+				
+				
+		
+		
+		
+		
+			
+		
+		
 
-        return max(0, self.rank-bp.rank)
-
-
-def TrainModel(csv_gen, default_rank = 0.5, player_dict = None):
+def TrainModel(csv_gen, default_rank = 0, player_dict = None):
     """Trains the model based on receiving a 'csv-generator' from the rows"""
 
     if player_dict is None:
-        player_dict = defaultdict(lambda : Player(rank = default_rank))
+        player_dict = PlayerDict(default_rank = default_rank)
 
     for row in csv_gen:
         p1 = int(row["White Player #"])
         p2 = int(row["Black Player #"])
-        s = float(row["Score"])
-
-        player_dict[p1].match(player_dict[p2], s)
+        s = InTreatScore(float(row["Score"]))
+		player_dict.PerformMatch(p1, p2, s)
 
     return player_dict
 
@@ -55,7 +133,7 @@ def EvaluateModel(model_dict, csv_gen):
         p1 = int(row["White Player #"])
         p2 = int(row["Black Player #"])
         s = float(row["Score"])
-        score = model_dict[p1].get_match_score(model_dict[p2])
+        score = model_dict.GetMatchScore(p1, p2)
         predicted_agg[(row['Month #'], p1, p2)] += score
         predicted_agg[(row['Month #'], p2)] += score
 
@@ -76,9 +154,14 @@ def WritePrediction(model_dict, csv_gen, out_handle):
         p2 = int(row["Black Player #"])
         m = row['Month #']
         score = model_dict[p1].get_match_score(model_dict[p2])
-        out_handle.write('%s,%i,%i,%f\n' % (m, p1, p2, score))
+        out_handle.write('%s,%i,%i,%f\n' % (m, p1, p2, OutTreatScore(score)))
 
-
+def InTreatScore(inscore):
+	return (inscore - 0.5)*2
+	
+def OutTreatScore(inscore):
+	return (inscore/2)+0.5
+	
 
 if __name__ == '__main__':
 
