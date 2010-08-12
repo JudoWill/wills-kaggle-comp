4bb82023964329469e5caaa28ad94fb97e876a49
Will Dampier <judowill@gmail.com>
2010-08-12 17:32:54 -0400

    MASE is correct now

--- tourism/process.py
+++ tourism/process.py
@@ -1,6 +1,15 @@
+from __future__ import division
 import numpy
 
 
 def CalculateMASE(test_in, cor_in):
 
-    pass
+    try:
+        abs_error = abs(test_in - cor_in)
+    except TypeError:
+        abs_error = abs(numpy.fromiter(test_in, numpy.float) - numpy.fromiter(cor_in, numpy.float))
+
+    mae = abs_error.mean()
+
+    scaled_errors = abs_error/mae
+    return scaled_errors.mean()
