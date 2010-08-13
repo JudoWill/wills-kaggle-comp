bb63ee8fbd42e1bb2bec16e6b2afc964d6bea260
Will Dampier <judowill@gmail.com>
2010-08-13 14:29:02 -0400

    added a setup to get the example data out

--- tourism/process.py
+++ tourism/process.py
@@ -1,5 +1,8 @@
 from __future__ import division
+import csv
 import numpy
+from operator import itemgetter
+from itertools import imap, chain
 
 
 def CalculateMASE(test_in, cor_in):
@@ -13,3 +16,24 @@ def CalculateMASE(test_in, cor_in):
 
     scaled_errors = abs_error/mae
     return scaled_errors.mean()
+
+
+def FloatConv(string):
+    try:
+        v = float(string)
+    except ValueError:
+        v = numpy.NaN
+    return v
+
+
+if __name__ == '__main__':
+
+
+    with open('initial-data/tourism_data.csv') as handle:
+        keys = map(lambda x:"Y%i"%x, range(1,518))
+        string_data = imap(itemgetter(*keys), csv.DictReader(handle))
+        data = imap(FloatConv, chain.from_iterable(string_data))
+        test_data = numpy.fromiter(data, numpy.float)
+
+    test_data = numpy.reshape(test_data, (-1, len(keys)))
+
